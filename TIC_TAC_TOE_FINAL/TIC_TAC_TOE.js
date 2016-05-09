44cece7e71532e504878484ce67b0c74d49826a4
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-08 19:59:48 -0700

    2nd draft

---
+++ TIC_TAC_TOE_FINAL/TIC_TAC_TOE.js
@@ -0,0 +1,40 @@
+window.onload = startGame();
+
+// var playerA, playerB, message, reset, winner;
+
+
+function startGame() {
+    xListener();
+    xListener();
+}
+
+function xListener(){
+    var cells = document.getElementsByTagName("td");
+        cells.addEventListener("click", console.log("clicked."))
+}
+
+function oListener(){
+
+}
+
+
+
+ window.onload = startGame();
+
+var cells = document.getElementsByTagName("td");
+
+function startGame() {
+    xoListener();
+};
+
+
+
+
+
+
+
+
+
+
+
+
