a368d74ba04ca685062de725e2c764470c23371c
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-13 13:34:16 -0700

    updated board links to js

--- TTT_revised.js
+++ TTT_revised.js
@@ -1,48 +1,34 @@
 console.log("hello world");
-window.onload = startGame();
-
+// window.onload = startGame();
+$(document).ready(function() {
+console.log('onready');
+startGame();
 
 // var xMove;
 // var oMove;
 
+});
 
 function startGame() {
 	$("#reset").on("click", function() {
 	// alert("working!");
 	$("#ui_message").text("Player X, you're up!");
 	// and reset
-	// playerBoardMove();
+	playerBoardMove();
 	});
 }
-function playerBoardMove() {
-	// if a cell is clicked, 
-	// 	toggle between x and O 
-	// when the next player goes, don't allow them to change board
-	// if x is chosen or === x
-	// 	then change to = o
-	// then vice versa
-	$("td.cell").each("click", function() {
-	// identify all the cells
-	if ($("td.cell")) === "X" {
-		($("td.cell")) = "O";
-	} else {
-		($("td.cell")) = "X";
-	// $("td#c1.cell").on("click", function() {
-	// identify all the cells
-	// $("td#c1.cell").text("X");
-	// $(this).toggleClass("cell"), make this a global perhaps? or just for cells
-	// console.log("I worked!");
-  }
-  });
-}
 
 
 function playerBoardMove() {
-	$("td.cell").on("click", function() {
-	if ($("td.cell") === "X") {
-		($("td.cell") = "O");
-	} else {
-		($("td.cell") = "X");
-  }
+	if $(".cell").on("click", function(e) {
+
+		console.log("clicked", e.target);
+	// if ($("td.cell") === "X") {
+	// 	($("td.cell").html("O");
+	// 	// ($("td.cell") = "O");
+	// } else {
+	// 	($("td.cell") = "X");
+ //  }
   });
 }
+
