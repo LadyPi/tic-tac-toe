45516fb254a00002d08418ed60858359aa3d2c86
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-14 19:33:48 -0700

    refactored TTT_revised js

--- TTT_revised.js
+++ TTT_revised.js
@@ -1,34 +1,38 @@
 console.log("hello world");
-// window.onload = startGame();
 $(document).ready(function() {
-console.log('onready');
 startGame();
-
-// var xMove;
-// var oMove;
+// playerX = "X";
+// playerO = "O";
 
 });
 
 function startGame() {
 	$("#reset").on("click", function() {
-	// alert("working!");
 	$("#ui_message").text("Player X, you're up!");
-	// and reset
-	playerBoardMove();
-	});
+	// reset
+   });
 }
-
-
-function playerBoardMove() {
-	if $(".cell").on("click", function(e) {
-
-		console.log("clicked", e.target);
-	// if ($("td.cell") === "X") {
-	// 	($("td.cell").html("O");
-	// 	// ($("td.cell") = "O");
-	// } else {
-	// 	($("td.cell") = "X");
- //  }
-  });
+function playerBoard() {
+	$(".cell").on("click", function(e) {
+	// console.log("clicked", e.target);
+	// console.log("I'm coming from playerBoard.");
+});
 }
-
+$(function() {
+	$("td").each(function playerMove() {
+		var ids = this.id;
+		$(this).on("click", function(e) {
+		if($(this).is(":empty"));
+			$(this).on("click", function(e) {
+				$(this).text("X");
+		if ($(this).is("O"));
+  			alert("This square is already being used.");
+		// if$(this).dblclick(function() {
+  // 			$(this).text("O");
+		// alert("I am a test.");
+});
+});
+});
+});
+ 
+		
